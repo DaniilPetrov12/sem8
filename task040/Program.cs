f5b7b30362ee208292c37ddea77e92dca9e10da8
Daniil Petrov <daniilp412@gmail.com>
2023-05-27 14:13:16 +0500

    sem6

---
+++ task040/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник со сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+
+System.Console.WriteLine("Введите первое число a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите первое число b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите первое число c = ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a + b > c && a+c>b && c+b >a)
+{
+    System.Console.WriteLine("Треугольник существует");
+}
+else{
+    System.Console.WriteLine("Не существует");
+}
---
+++ task044/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+System.Console.Write("Введите  число a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+int fib = 0;
+int num1 = 0;
+int num2 = 1;
+System.Console.Write("0 1 ");
+for (int i = 0;i <a; i++)
+{
+   fib = num1 + num2;
+   num1=num2;
+   num2 = fib;
+   System.Console.Write($" {fib} " );
+}
