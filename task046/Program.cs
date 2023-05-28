4494705b93e68bab7cc9c647129d737d3e627ba8
Daniil Petrov <daniilp412@gmail.com>
2023-05-28 12:52:40 +0500

    sem7+

---
+++ task046/Program.cs
@@ -0,0 +1,29 @@
+﻿// задайте двумерный массив заполненый случайными числами
+
+void FillMatrix(int [,] matrix)
+{
+    Random rnd = new Random();
+for (int i =0; i< matrix.GetLength(0);i++)
+{
+    for ( int j = 0; j < matrix.GetLength(i);j++)
+    matrix[i,j] = rnd.Next(0 , 11);
+}
+}
+void PrintMatrix (int [,] matrix)
+{
+
+for (int i =0; i< matrix.GetLength(0);i++)
+{
+    for ( int j = 0; j < matrix.GetLength(i);j++)
+    System.Console.WriteLine(($"{matrix [i,j]}"));
+} 
+}
+System.Console.WriteLine("Введите количество строк массива");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите количество столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int [,] matrix = new int [m,n];
+
+FillMatrix(matrix);
+PrintMatrix(matrix);
---
+++ task048/Program.cs
@@ -0,0 +1,29 @@
+﻿// задайте массив размера m на n в котором элементы находятся по формуле i+j
+
+void FillMatrix(int [,] matrix)
+{
+    
+for (int i =0; i< matrix.GetLength(0);i++)
+{
+    for ( int j = 0; j < matrix.GetLength(i);j++)
+    matrix[i,j] = i +j;
+}
+}
+void PrintMatrix (int [,] matrix)
+{
+
+for (int i =0; i< matrix.GetLength(0);i++)
+{
+    for ( int j = 0; j < matrix.GetLength(i);j++)
+    System.Console.WriteLine(($"{matrix [i,j]}"));
+} 
+}
+System.Console.WriteLine("Введите количество строк массива");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите количество столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int [,] matrix = new int [m,n];
+
+FillMatrix(matrix);
+PrintMatrix(matrix);
